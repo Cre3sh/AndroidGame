26d07077e5910fd594d55b4764131845845f28bf
Cre3sh <creesh1990@gmail.com>
2025-02-26 16:38:15 +0000

    Initial commit

---
+++ app/src/main/java/com/example/androidgame/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.androidgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.EditText;
+
+import com.example.androidgame.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'androidgame' library on application startup.
+    static {
+        System.loadLibrary("androidgame");
+    }
+
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        EditText UsernameField = binding.sampleText;
+        UsernameField.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'androidgame' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
